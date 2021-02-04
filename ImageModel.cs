6ee1806bed1c0408093b860905079cb70a4ab866
TengshengHou <tengsheng.hou@lexisnexis.com>
2021-02-04 11:05:46 +0800

    first commit

---
+++ ImageModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ImageToBase64
+{
+    public class ImageModel
+    {
+        public string Path { get; set; }
+        public string FullPath { get; set; }
+        public string Base64Data
+        {
+            get
+            {
+                return this.Convert();
+            }
+        }
+
+        public string VariableName
+        {
+            get
+            {
+                return this.Path.Replace("/", "_").Replace("\\", "_").Replace(".", "_");
+            }
+        }
+
+        private string Convert()
+        {
+            var fileExtension = System.IO.Path.GetExtension(this.FullPath).ToLower().Replace(".", "");
+            var base64data = ImageToBase64(this.FullPath);
+            return $"data:image/{fileExtension};base64,{base64data}";
+        }
+        public static string ImageToBase64(string fileFullName)
+        {
+            Bitmap bmp = null;
+            try
+            {
+                var suffix = System.IO.Path.GetExtension(fileFullName).ToLower();
+                var suffixName = suffix == ".png" ? ImageFormat.Png
+                    : suffix == ".jpg" || suffix == ".jpeg"
+                        ? ImageFormat.Jpeg
+                        : suffix == ".bmp"
+                            ? ImageFormat.Bmp
+                            : suffix == ".gif"
+                                ? ImageFormat.Gif
+                                : ImageFormat.Jpeg;
+
+                string result = string.Empty;
+                bmp = new Bitmap(fileFullName);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    bmp.Save(ms, suffixName);
+                    byte[] arr = new byte[ms.Length];
+                    ms.Position = 0;
+                    ms.Read(arr, 0, (int)ms.Length);
+                    ms.Close();
+                    result = System.Convert.ToBase64String(arr);
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                if (bmp != null)
+                    bmp.Dispose();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace ImageToBase64
+{
+
+    class Program
+    {
+        static List<ImageModel> imageModels = new List<ImageModel>();
+        static string basePath;
+        static void Main(string[] args)
+        {
+            basePath = args.Length > 0 ? args[0] : Directory.GetCurrentDirectory();
+            var htmlFilesPath = GetHtmlFiles(basePath);
+            foreach (var path in htmlFilesPath)
+            {
+                FindImgSrcFromFile(path);
+                ReplaceImgSrc(path);
+            }
+            CreateImageTsFile(imageModels);
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        static void FindImgSrcFromFile(string htmlFilePath)
+        {
+            var htmlContent = File.ReadAllText(htmlFilePath);
+            string pattern = @"<img\b[^<>]*?\bsrc[\s\t\r\n]*=[\s\t\r\n]*[""']?[\s\t\r\n]*(?<imgUrl>[^\s\t\r\n""'<>]*)[^<>]*?/?[\s\t\r\n]*>";
+            foreach (Match match in Regex.Matches(htmlContent, pattern))
+            {
+                if (match.Groups.Count < 1)
+                {
+                    continue;
+                }
+                var imagePath = match.Groups[1].Value;
+                if (imageModels.Any(image => image.Path == imagePath))
+                {
+                    continue;
+                }
+
+                imageModels.Add(new ImageModel
+                {
+                    Path = imagePath,
+                    FullPath = Path.Combine(basePath, match.Groups[1].Value)
+                });
+            }
+        }
+
+        static void ReplaceImgSrc(string htmlFilePath)
+        {
+            var htmlContent = File.ReadAllText(htmlFilePath);
+            foreach (var model in imageModels)
+            {
+                htmlContent = htmlContent.Replace(model.Path, $"{{{{images.{model.VariableName}}}}}");
+            }
+            File.WriteAllText($"{htmlFilePath}.new.html", htmlContent);
+        }
+
+        static List<string> GetHtmlFiles(string directoryPath)
+        {
+            return Directory.GetFiles(directoryPath, "*.html").ToList();
+        }
+
+        static void CreateImageTsFile(List<ImageModel> imageModels)
+        {
+            string fileName = "images.ts";
+            fileName = Path.Combine(basePath, fileName);
+            if (File.Exists("images.ts"))
+            {
+                File.Delete(fileName);
+
+            }
+            var lines = new List<string>();
+            foreach (var item in imageModels)
+            {
+                lines.Add($"public static readonly {item.VariableName}: string = `{item.Base64Data}`;");
+            }
+            File.WriteAllLines(fileName, lines);
+        }
+    }
+}
